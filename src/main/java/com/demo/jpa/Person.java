961e2021da3555cda0c9149f65a5769310e1e9e4
jcdominguez <jcd.pro@gmail.com>
2024-01-16 14:16:34 +0100

    JPA @Column

--- src/main/java/com/demo/jpa/Person.java
+++ src/main/java/com/demo/jpa/Person.java
@@ -10,7 +10,9 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name="first_name")
     private String firstName;
+    @Column(name="last_name")
     private String lastName;
 
     public Person() {
