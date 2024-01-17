eb59fe309edd110f58127260d681e487a66d6cd7
jcdominguez <jcd.pro@gmail.com>
2024-01-17 10:57:26 +0100

    @ManyToMany

---
+++ src/main/java/com/demo/jpa/StageDAO.java
@@ -0,0 +1,81 @@
+package com.demo.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
+
+import java.util.List;
+
+public class StageDAO {
+
+    public static void save(Stage a){
+        EntityManager entityManager = EntityManagerSingleton.getEntityManager();
+
+       EntityTransaction tx = entityManager.getTransaction();
+       try {
+            tx.begin();
+            entityManager.persist(a);
+            tx.commit();
+        }catch (Exception e){
+            System.out.println("Erreur lors de l'enregistrement");
+            tx.rollback();
+        }
+    }
+
+    public static Address findById(Integer id){
+        EntityManager entityManager = EntityManagerSingleton.getEntityManager();
+
+        return entityManager.find(Address.class, id);
+    }
+
+    public static List<Address> findAll(){
+        EntityManager entityManager = EntityManagerSingleton.getEntityManager();
+
+        // JPQL : sorte de requete SQL mais avec les classes Java
+        Query query  = entityManager.createQuery("SELECT a FROM Stage a");// SELECT * FROM persons;
+        return query.getResultList();
+    }
+
+
+    public static void delete(Address a){
+        EntityManager entityManager = EntityManagerSingleton.getEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            entityManager.remove(a);
+            tx.commit();
+        }catch (Exception e){
+            System.out.println("Erreur lors de la suppression");
+            tx.rollback();
+        }
+    }
+
+    public static void deleteById(Integer id){
+        EntityManager entityManager = EntityManagerSingleton.getEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            Query query  = entityManager.createQuery("DELETE FROM Stage a WHERE a.id = :id");
+            query.setParameter("id", id);
+            query.executeUpdate();
+            tx.commit();
+        }catch (Exception e){
+            System.out.println("Erreur lors de la modification");
+            tx.rollback();
+        }
+    }
+
+
+    public static void update(Stage a){
+        EntityManager entityManager = EntityManagerSingleton.getEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            entityManager.merge(a);
+            tx.commit();
+        }catch (Exception e){
+            System.out.println("Erreur lors de la modification");
+            tx.rollback();
+        }
+    }
+}
--- src/test/java/com/demo/AppTest.java
+++ src/test/java/com/demo/AppTest.java
@@ -2,11 +2,10 @@ package com.demo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.demo.jpa.Address;
-import com.demo.jpa.AddressDAO;
-import com.demo.jpa.Person;
-import com.demo.jpa.PersonDAO;
+import com.demo.jpa.*;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 public class AppTest
 {
@@ -24,5 +23,30 @@ public class AppTest
         Person marie = new Person("Marie", "Dupont");
         marie.setAddress(address);
         PersonDAO.save(marie);
+    }
+
+    @Test
+    public void testStage(){
+        Person marie = new Person("Marie", "Dupont");
+        PersonDAO.save(marie);
+
+        Person alain = new Person("Alain", "Dufour");
+        PersonDAO.save(alain);
+
+        Person said = new Person("Said", "Dominguez");
+        PersonDAO.save(said);
+
+        Stage salsa = new Stage("Stage Salsa", "pratique avancée de la Salsa",
+                LocalDateTime.of(2024, 12, 13, 9,30));
+        salsa.addIntern(marie);
+        salsa.addIntern(alain);
+        StageDAO.save(salsa);
+
+        Stage java = new Stage("Java pour les nuls", "les fondamentaux du langage",
+                LocalDateTime.of(2024, 4, 13, 9,30));
+        java.addIntern(alain);
+        java.addIntern(said);
+        StageDAO.save(java);
+
     }
 }
